e01726a6cef13ce9030d111a3b0e3075df9416b5
Mogitures <davibeserker@outlook.com.br>
2024-08-12 20:18:16 -0300

    version3

---
+++ clone.php
@@ -0,0 +1,19 @@
+<?php 
+
+	// sprintf s = string f = format
+
+	print "Usando print para exibir na tela <br/>";
+
+	$data = "20 de Agosto de 2020";
+	$salario = 850.00;
+	$cargo = "Estagiário";
+
+	echo "Arquivo criado em $data <br/>";
+
+	printf("Salário mínimo: R$ %.2f <br/>", $salario);
+
+	$texto = sprintf("%s recebe R$ %.2f por mês", $cargo, $salario);
+
+	echo $texto;
+
+ ?>
